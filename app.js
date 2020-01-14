80d92f822382740af1fa49a2a14b60cb7e493da9
Mason <liujianfeng@kingdee.com>
2020-01-14 18:43:27 +0800

    删除不需要的文件

--- app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import { Button } from 'antd'
-const App = () => {
-    return <Button>123</Button>
-}
-
-ReactDom.render(<App />, document.querySelector('#root'))
